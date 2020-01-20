90f7b8b37a02c69cf2c831dc4106c9e48e53f463
jatin33 <jatinpanjwani12@gmail.com>
2020-01-21 00:41:03 +0530

    Redesigned home page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,13 +3,15 @@ import SocialMedia from '../SocialMedia/SocialMedia';
 import AutoType from '../AutoType/AutoType';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { MdFileDownload } from 'react-icons/md';
 
 const FlexRow = styled.div`
 display:flex;
 flex-direction:row;
 justify-content: space-evenly;
 color : white;
-margin:0.2em;
+margin-bottom:0.5em;
+max-height: 20em;
 padding:0.5em;
 `;
 
@@ -20,52 +22,85 @@ padding-top:1em;
 padding-left:0.5em;
 flex-direction:column;
 justify-content:space-evenly;
-height: 75vh;
+height: 95vh;
 `;
 
 const SocialMediaRow = styled(FlexRow)`
-justify-content:space-around; 
-height: 25vh;
+justify-content:flex-end; 
 `;
 
-const NameColumn = styled(FlexColumn)`
-flex:2;
-`;
 
 const StyledLink = styled(Link)`
     font-weight:bold;
-    color: antiquewhite;
+    color: rgba(76,187,23);
     text-decoration:none;
     display:inline-block;
-    z-index:99;
+    position : relative;
+    z-index:9;
+
+    &:hover{
+     text-decoration: none;   
+    }
+
+    &:after {
+        border-radius: 1em;
+        border-top: .1em solid rgba(76,187,23);
+        content: "";
+        position: absolute;
+        right: 100%;
+        top: 1.5em;
+        left: 0;
+        transition: right .4s cubic-bezier(0,.5,0,1);
+    }
+
+    &:hover:after {
+        right: 0;
+      }
 `;
 
 const StyledPara = styled.p`
+ text-align:left;
+ line-height: 1.4em;
  font-weight: bold;
  font-style: italic;
+ max-width: 35em;
+`;
+
+const DownloadLink = styled.a`
+position: relative;
+font-weight:bold;
+font-size:1em;
+background-color: whitesmoke;
+color : #000906;
+padding: 0.5em;
+margin: 0.5em; 
+text-decoration: none;
+z-index: 9;
 `;
 
 function About(){
     return(
       <FlexColumn>
-        <FlexRow>
-            <FlexColumn>
+         <FlexRow>
                 <StyledLink to='/experiences'>Experiences</StyledLink>
                 <StyledLink to='/skills'>Skills</StyledLink>
-            </FlexColumn>
-            <NameColumn>
-                <AutoType text={'Jatin Panjwani, Software Developer'}/>
-            </NameColumn>
-            <FlexColumn>
                 <StyledLink to='/'>About</StyledLink>
                 <StyledLink to='/opensource'>Open Source</StyledLink>
-            </FlexColumn>
+         </FlexRow>
+         <FlexRow>
+                <AutoType text={'Jatin Panjwani, Software Developer'}/>
          </FlexRow>
          <FlexRow>
              <StyledPara>
                  I'm passionate Full Stack Developer who believes in building beautiful UI with robust, scalable backends.
-                 I have realized the power of giving back to community, hence started contributing to Open Source.
+                 I have realized the power of giving back to community, hence started contributing to Open Source. I am currently fascinated by JavaScript ecosystem.
              </StyledPara>
+         </FlexRow>
+         <FlexRow>
+             <DownloadLink href="https://drive.google.com/uc?export=download&id=1wxd-DKbEgCLO7mNo99A7ytFgKaOEj0hS">
+             <MdFileDownload/>
+                    {'  Resume'}
+             </DownloadLink>
          </FlexRow>
          <SocialMediaRow>
             <SocialMedia/>
--- src/components/SocialMedia/SocialMedia.js
+++ src/components/SocialMedia/SocialMedia.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import { FaFacebook, FaLinkedin, FaGithub} from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
 import styled from 'styled-components';
 
 const IconLinks = styled.a`
     display: inline-block;
-    margin: 1em;
+    margin-left:0.5em;
+    margin-right:0.5em;
     text-decoration: none;
+    color: rgba(76,187,23);
     z-index:999;
 `
 
 function SocialMedia() {
     return (
         <>
+            <IconLinks href = "mailto:jatinpanjwani111@gmail.com">
+             <MdEmail size={42}/>   
+            </IconLinks>
             <IconLinks href="https://www.facebook.com/panjwani.jatin">    
-            <FaFacebook size={56}/>
+            <FaFacebook size={42}/>
             </IconLinks>
             <IconLinks href="https://www.linkedin.com/in/jatinpanjwani">    
-            <FaLinkedin size={56}/>
+            <FaLinkedin size={42}/>
             </IconLinks>
             <IconLinks href="https://www.github.com/jatin33">    
-            <FaGithub size={56}/>
+            <FaGithub size={42}/>
             </IconLinks>
         </>
     )
